540abad875c8ea3f51634cbc26e5a813d192016f
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-02-08 15:42:52 +0530

    feat/templete created

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -9,44 +9,54 @@ const Grid = () => {
         [1,1,1]
     ];
  
-    const [color,setColor]=useState([]);
-    const [dummy,setDummy]=useState([]);
-    const [indexToDelete, setIndexToDelete] = useState(0);
+    const [color,setColor]=useState(new Set());
+    const [count,setCount]=useState(0);
+    // const [indexToDelete, setIndexToDelete] = useState(0);
 
 
-    const handleClick=(index)=>{
+   
+    // useEffect(()=>{
+    //   if(dummy.length>=8){
+   
+    //      const t=setInterval(()=>{
       
-      //  setDummy((prev)=>[...prev,index]);
-    }
+    //     const newDummy = [...dummy];
+    //        newDummy.splice(indexToDelete,1);
+    //        setDummy(newDummy);
+    //        setIndexToDelete(indexToDelete+1);
+    //        console.log(dummy);
+    //     },1000) 
+
+    //  return ()=>clearTimeout(t);
+
+    //   }
 
     useEffect(()=>{
-      if(dummy.length>=8){
-   
-         const t=setInterval(()=>{
-      
-        const newDummy = [...dummy];
-           newDummy.splice(indexToDelete,1);
-           setDummy(newDummy);
-           setIndexToDelete(indexToDelete+1);
-           console.log(dummy);
-        },1000) 
+       
+      if(count>=8){
+         
+          const colorArray = Array.from(color);
+          const lastElement = colorArray[colorArray.length - 1];
+          console.log(lastElement);
+     }
+    },[count])
 
-     return ()=>clearTimeout(t);
-
-      }
-    },[dummy])
+    const handleOnOff=(index)=>{
+        console.log(`on/off ${index}`);
+        setColor(new Set([...color,index]));
+        setCount(count+1);
+    }
 
   return (
-    <div className='h-72 w-72 border border-black items-center p-2 grid grid-cols-3'>
+    <div className='h-72 w-72 border-2 border-black rounded items-center p-2 grid grid-cols-3'>
        {
         config.flat(1).map((data,index)=>{
             return(
-                
                 data?
-                <div onClick={()=>handleClick(index)} key={index} className=''>
-                   <div className={`h-20 w-20 border border-black ${data==2?"bg-green-400":"bg-white"}`}></div>
-                </div>:<span key={index}></span>
-                
+                <div onClick={()=>handleOnOff(index)} key={index} >
+                   <div className={`h-20 w-20 border border-black  ${color.has(index)?"bg-green-500":""}` }></div>
+                </div>
+                :<span key={index}></span>                
             )
         })
        }
